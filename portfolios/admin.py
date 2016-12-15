0148438098a48161b75832b7d054337dfd7036c9
renato muniz <renato-muniz@hotmail.com>
2016-12-15 16:37:55 -0300

    viewes e urls

---
+++ portfolios/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from models import DadosPessoais
+# Register your models here.
+
+
+admin.site.register(DadosPessoais)
+
+
---
+++ portfolios/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def portfolio_exibir(request):
+    return render(request,'portfolios/portfolio_exibir.html',{})
